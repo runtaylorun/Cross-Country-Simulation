1f5fce96f65213b83bdb123af95e8726c031b24f
Taylor Mills <trmills@eagles.usi.edu>
2021-11-25 18:44:51 -0500

    re-add file

---
+++ src/Scripts/IndexedDb/seasonServices.js
@@ -0,0 +1,91 @@
+export const getWeek = (leagueName) => {
+  return new Promise((resolve, reject) => {
+    const openRequest = indexedDB.open(leagueName)
+
+    openRequest.onsuccess = (event) => {
+      const db = event.target.result
+
+      const transaction = db.transaction('Season', 'readwrite')
+
+      const objectStore = transaction.objectStore('Season')
+
+      const objectStoreRequest = objectStore.getAll()
+
+      objectStoreRequest.onsuccess = (event) => {
+        const season = objectStoreRequest.result.sort((a, b) => b.year - a.year)[0]
+        resolve(season.week)
+      }
+    }
+  })
+}
+
+export const getSeason = (leagueName) => {
+  return new Promise((resolve, reject) => {
+    const openRequest = indexedDB.open(leagueName)
+
+    openRequest.onsuccess = (event) => {
+      const db = event.target.result
+
+      const transaction = db.transaction('Season', 'readwrite')
+
+      const objectStore = transaction.objectStore('Season')
+
+      const objectStoreRequest = objectStore.getAll()
+
+      objectStoreRequest.onsuccess = (event) => {
+        const season = objectStoreRequest.result[objectStoreRequest.result.length - 1]
+        resolve(season)
+      }
+    }
+  })
+}
+
+export const getSeasonInfo = (leagueName) => {
+  return new Promise((resolve, reject) => {
+    const openRequest = indexedDB.open(leagueName)
+
+    openRequest.onsuccess = (event) => {
+      const db = event.target.result
+
+      const transaction = db.transaction('Season', 'readwrite')
+
+      const objectStore = transaction.objectStore('Season')
+
+      const objectStoreRequest = objectStore.get(1)
+
+      objectStoreRequest.onsuccess = (event) => {
+        const season = objectStoreRequest.result
+        resolve(season)
+      }
+    }
+  })
+}
+
+export const incrementSeasonWeek = (leagueName) => {
+  return new Promise((resolve, reject) => {
+    const openRequest = indexedDB.open(leagueName)
+
+    openRequest.onsuccess = (event) => {
+      const db = event.target.result
+
+      const transaction = db.transaction('Season', 'readwrite')
+
+      const objectStore = transaction.objectStore('Season')
+
+      const objectStoreRequest = objectStore.get(1)
+
+      objectStoreRequest.onsuccess = (event) => {
+        const season = objectStoreRequest.result
+
+        season.weeksLeft -= 1
+        season.currentWeek += 1
+
+        const updateSeasonRequest = objectStore.put(season, 1)
+
+        updateSeasonRequest.onsuccess = () => {
+          resolve('Week added')
+        }
+      }
+    }
+  })
+}
